0787d407bfa419fc1446a1f95299199db983c4fc
Jood <56412800+Jood80@users.noreply.github.com>
2023-01-09 19:33:45 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,4 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://dwrevelopers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
 	CLIENT_ID: string;
 }
 
@@ -37,7 +19,8 @@ const checkOrigin = (request: Request) => {
 	return foundOrigin ? foundOrigin : allowedOrigins[0]
 }
 
-const getImages = async (	request: Request,
+const getImages = async (
+	request: Request,
 	env: Env): Promise<Response> => {
 	const { query }: any = await request.json()
 	
@@ -47,7 +30,7 @@ const getImages = async (	request: Request,
 		}
 	})
 	
-	const data : any = await res.json() 
+	const data : any = await res.        json() 
 	const images = data.results.map((image:any) => ({
 		id: image.id,
 		image: image.urls.small,
